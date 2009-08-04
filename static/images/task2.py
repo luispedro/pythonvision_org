17cc96f5235af94fb3d5c9220c31de4d9fcca8e6
Luís Pedro Coelho <lpc@cmu.edu>
2009-08-04 18:00:08 -0400

    Task 2.
    This allows me to make sure that the code I recommend actually runs

---
+++ static/images/task2.py
@@ -0,0 +1,68 @@
+import numpy as np
+import scipy
+import pylab
+import pymorph
+import readmagick
+from scipy import ndimage
+import pyslic
+dna = readmagick.readimg('dna.jpeg')
+
+TESTING = True
+if TESTING:
+    def savejet(img, name):
+        pass
+    def writeimg(img, name):
+        pass
+else:
+    def savejet(img):
+        readmagick.writeimg((cm.jet(dnaf)[:,:,:3]*255).astype(np.uint8), name)
+    writeimg = readmagick.writeimg
+
+dnaf = ndimage.gaussian_filter(dna, 8)
+rmax = pymorph.regmax(dnaf)
+pylab.imshow(pymorph.overlay(dna, rmax))
+pylab.show()
+writeimg(pymorph.overlay(dna, rmax), 'dnaf-rmax-overlay.jpeg')
+savejet(dnaf, 'dnaf-8.jpeg')
+
+
+dnaf = ndimage.gaussian_filter(dna, 16)
+rmax = pymorph.regmax(dnaf)
+pylab.imshow(pymorph.overlay(dna, rmax))
+pylab.show()
+writeimg(pymorph.overlay(dna, rmax), 'dnaf-16-rmax-overlay.jpeg')
+
+seeds,nr_nuclei = ndimage.label(rmax)
+print nr_nuclei
+
+T = pyslic.thresholding.otsu(dnaf)
+dist = ndimage.distance_transform_edt(dnaf > T)
+dist = dist.max() - dist
+dist = ((dist - dist.min())/float(dist.ptp())*255).astype(np.uint8)
+pylab.imshow(dist)
+pylab.show()
+
+savejet(dist, 'dnaf-16-dist.jpeg')
+
+nuclei = pymorph.cwatershed(dist, seeds)
+pylab.imshow(nuclei)
+pylab.show()
+savejet(nuclei, 'nuclei-segmented.png')
+
+
+whole = pyslic.segmentation.gvoronoi(nuclei)
+pylab.imshow(whole)
+pylab.show()
+savejet(whole, 'whole-segmented.png')
+
+borders = np.zeros(nuclei.shape, np.bool)
+borders[ 0,:] = 1
+borders[-1,:] = 1
+borders[:, 0] = 1
+borders[:,-1] = 1
+at_border = np.unique(nuclei[borders])
+for obj in at_border:
+    whole[whole == obj] = 0
+pylab.imshow(whole)
+pylab.show()
+savejet(whole, 'whole-segmented-filtered.png')
