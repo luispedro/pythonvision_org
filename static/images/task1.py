8d37b9bd3a340970536981bd1ad567f041b95bcd
Luís Pedro Coelho <lpc@cmu.edu>
2009-08-04 17:47:20 -0400

    Test the code from the tutorial
    (As a side effect, save the images)

---
+++ static/images/task1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import scipy
+import pylab
+import pymorph
+import readmagick
+from scipy import ndimage
+dna = readmagick.readimg('dna.jpeg')
+
+TESTING = True
+if TESTING:
+    def savejet(img, name):
+        pass
+else:
+    def savejet(img):
+        readmagick.writeimg((cm.jet(dnaf)[:,:,:3]*255).astype(np.uint8), name)
+    
+pylab.imshow(dna)
+pylab.show()
+savejet(dna, 'dna-coloured.jpeg')
+
+pylab.imshow(dna)
+pylab.gray()
+pylab.show()
+
+print dna.shape
+print dna.dtype
+print dna.max()
+print dna.min()
+
+pylab.imshow(dna // 2)
+pylab.show()
+
+import pyslic
+T = pyslic.thresholding.otsu(dna)
+pylab.imshow(dna > T)
+pylab.show()
+
+dnaf = ndimage.gaussian_filter(dna, 8)
+T = pyslic.thresholding.otsu(dnaf)
+pylab.imshow(dnaf > T)
+pylab.show()
+
+labeled,nr_objects = ndimage.label(dnaf > T)
+print nr_objects
+pylab.imshow(labeled)
+pylab.jet()
+pylab.show()
+savejet(labeled, 'dnaf-otsu-labeled.jpeg')
