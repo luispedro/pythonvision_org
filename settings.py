4ad7d4eb762fc7053d5b04a10049276e3f8523d6
Luis Pedro Coelho <lpc@cmu.edu>
2010-04-18 19:24:41 -0400

    Everything is called pythonvision in this project

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ APPEND_SLASH = True
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'                     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = _BASE_DIR + '/python_vision.db'     # Or path to database file if using sqlite3.
+DATABASE_NAME = _BASE_DIR + '/pythonvision.db'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
