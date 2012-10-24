fb9bc63bcd29d6b84c679e69beafa0b16f854f56
Luis Pedro Coelho <luis@luispedro.org>
2012-10-24 18:32:42 +0100

    ENH Update to newer django & gitcms

--- settings.py
+++ settings.py
@@ -9,13 +9,12 @@ ADMINS = (
 )
 APPEND_SLASH = True
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'sqlite3'                     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = _BASE_DIR + '/pythonvision.db'     # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': _BASE_DIR + '/pythonvision.db',
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -83,9 +82,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.markup',
 
-    'gitcms.simpletagging',
-    'gitcms.simplecms',
-    'gitcms.simplemenus',
+    'gitcms.tagging',
+    'gitcms.pages',
+    'gitcms.menus',
     'gitcms.blog',
     'gitcms.files',
     'gitcms.redirect',
